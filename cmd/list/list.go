2bd04da6f61cad0a1955f64369809616ce71eaa0
Shohi Wang <oshohi@gmail.com>
2020-03-26 18:38:08 +0800

    add list options (#9)

--- cmd/list/list.go
+++ cmd/list/list.go
@@ -4,16 +4,24 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"strings"
 
 	"github.com/spf13/cobra"
 
 	"github.com/shohi/cube/pkg/kube"
 )
 
+// Options for list subcommand
+type Options struct {
+	Name string // name filter
+}
+
 // New creates a new `list` subcommand.
 // Output format
 // cluster_name : `ssh-forwarding`
 func New() *cobra.Command {
+	var opts = &Options{}
+
 	c := &cobra.Command{
 		Use:     "list",
 		Aliases: []string{"ls"},
@@ -25,11 +33,52 @@ func New() *cobra.Command {
 				return err
 			}
 
-			content, _ := json.MarshalIndent(l, "", "  ")
+			selected := filter(l, ByName(opts.Name))
+
+			content, _ := json.MarshalIndent(selected, "", "  ")
 			fmt.Println(string(content))
 			return nil
 		},
 	}
 
+	setupFlags(c, opts)
+
 	return c
+}
+
+func filter(s kube.ClusterInfos, fn FilterFunc) kube.ClusterInfos {
+	result := make(kube.ClusterInfos, 0, len(s))
+
+	for _, c := range s {
+		if fn(c) {
+			result = append(result, c)
+		}
+	}
+
+	return result
+}
+
+type FilterFunc func(kube.ClusterInfo) bool
+
+func EnableAll(_ kube.ClusterInfo) bool {
+	return true
+}
+
+func ByName(name string) FilterFunc {
+	n := strings.TrimSpace(name)
+
+	if n == "" {
+		return EnableAll
+	}
+
+	return func(c kube.ClusterInfo) bool {
+		return strings.Contains(c.Name, n)
+	}
+}
+
+// setupFlags sets flags for comand line
+func setupFlags(cmd *cobra.Command, opts *Options) {
+	flagSet := cmd.Flags()
+
+	flagSet.StringVar(&opts.Name, "name", "", "cluster name pattern")
 }
